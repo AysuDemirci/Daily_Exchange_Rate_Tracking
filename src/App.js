6114fc870be58e8f8a0e8085f61cad671dabf09b
Aysu <aysu.gunerr@gmail.com>
2022-12-05 23:57:38 +0300

    added animation to exchange rates

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import CarouselExhange from "./Components/CarouselExchange";
 import SideNavbar from "./Components/SideNavbar";
 import Contents from "./Components/Contents";
 import "./Style.css";
 import { Col, Container, Row } from "reactstrap";
 
-function App() {
-  return (
-    <div>
+export default class App extends Component {
+  state = {
+    exchangeRates: [],
+  };
+
+  getExchangeRates = () => {
+    fetch("http://localhost:3000/exchangeRates")
+      .then((response) => response.json())
+      .then((data) => this.setState({ exchangeRates: data }));
+    console.log(this);
+  };
+
+  render() {
+    return (
       <div>
-        <h4 className="header">Daily Exchange Rate Tracking</h4>
+        <div>
+          <h4 className="header">Daily Exchange Rate Tracking</h4>
+        </div>
+
+        <br />
+        <CarouselExhange
+          getExchangeRates={this.getExchangeRates}
+          exchangeRates={this.state.exchangeRates}
+        />
+        <br />
+        <br />
+        <br />
+        <Container>
+          <Row>
+            <Col md="3">
+              <SideNavbar />
+            </Col>
+            <Col md="9">
+              <Contents />
+            </Col>
+          </Row>
+        </Container>
       </div>
-
-      <br />
-      <CarouselExhange />
-      <br />
-      <br />
-      <br />
-      <Container>
-        <Row>
-          <Col md="3">
-            <SideNavbar />
-          </Col>
-          <Col md="9">
-            <Contents />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
--- src/Components/CarouselExchange.js
+++ src/Components/CarouselExchange.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Slider from "react-slick";
 import {
   Card,
@@ -12,146 +12,37 @@ import {
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-function CarouselExhange() {
-  var settings = {
-    infinite: true,
-    slidesToShow: 7,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    pauseOnHover: true,
-  };
+export default class CarouselExhange extends Component {
+  componentDidMount() {
+    this.props.getExchangeRates();
+  }
 
-  return (
-    <div>
-      <Container>
-        <Row className="exchange-card-styles">
-          <Slider {...settings}>
-            <Col>
-              <Card
-                style={{
-                  width: "150px",
-                  height: "100px",
-                  textAlign: "center",
-                  border: "1px solid black",
-                }}
-              >
-                <CardBody>
-                  <CardTitle>USD / TRY</CardTitle>
-                  <CardText>18,64</CardText>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col>
-              <Card
-                style={{
-                  width: "150px",
-                  height: "100px",
-                  textAlign: "center",
-                  border: "1px solid black",
-                }}
-              >
-                <CardBody>
-                  <CardTitle>EUR / TRY</CardTitle>
-                  <CardText>19,63</CardText>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col>
-              <Card
-                style={{
-                  width: "150px",
-                  height: "100px",
-                  textAlign: "center",
-                  border: "1px solid black",
-                }}
-              >
-                <CardBody>
-                  <CardTitle>GBP / TRY</CardTitle>
-                  <CardText>22,91</CardText>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col>
-              <Card
-                style={{
-                  width: "150px",
-                  height: "100px",
-                  textAlign: "center",
-                  border: "1px solid black",
-                }}
-              >
-                <CardBody>
-                  <CardTitle>AUD / TRY</CardTitle>
-                  <CardText>12,66</CardText>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col>
-              <Card
-                style={{
-                  width: "150px",
-                  height: "100px",
-                  textAlign: "center",
-                  border: "1px solid black",
-                }}
-              >
-                <CardBody>
-                  <CardTitle>CHF / TRY</CardTitle>
-                  <CardText>19,88</CardText>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col>
-              <Card
-                style={{
-                  width: "150px",
-                  height: "100px",
-                  textAlign: "center",
-                  border: "1px solid black",
-                }}
-              >
-                <CardBody>
-                  <CardTitle>NOK / TRY</CardTitle>
-                  <CardText>1,92</CardText>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col>
-              <Card
-                style={{
-                  width: "150px",
-                  height: "100px",
-                  textAlign: "center",
-                  border: "1px solid black",
-                }}
-              >
-                <CardBody>
-                  <CardTitle>GOLD gr. / TRY</CardTitle>
-                  <CardText>1075,45</CardText>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col>
-              <Card
-                style={{
-                  width: "150px",
-                  height: "100px",
-                  textAlign: "center",
-                  border: "1px solid black",
-                }}
-              >
-                <CardBody>
-                  <CardTitle>SILVER gr. / TRY</CardTitle>
-                  <CardText>17,81</CardText>
-                </CardBody>
-              </Card>
-            </Col>
-          </Slider>
-        </Row>
-      </Container>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <Container className="exchange-slideshow">
+          <Row className="exchange-card-styles">
+            <div style={{ display: "flex" }}>
+              {this.props.exchangeRates.map((exchangeRate) => (
+                <Col>
+                  <ul className="exchange-ul-style" key={exchangeRate.id}>
+                    <li>{exchangeRate.currencyName}</li>
+                    <li>{exchangeRate.price}</li>
+                  </ul>
+                </Col>
+              ))}
+              {this.props.exchangeRates.map((exchangeRate) => (
+                <Col>
+                  <ul className="exchange-ul-style" key={exchangeRate.id}>
+                    <li>{exchangeRate.currencyName}</li>
+                    <li>{exchangeRate.price}</li>
+                  </ul>
+                </Col>
+              ))}
+            </div>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
-
-export default CarouselExhange;
