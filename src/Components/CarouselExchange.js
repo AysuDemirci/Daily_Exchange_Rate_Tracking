68f2e840946d5106e2d9a75a1aaf212d29a49fc3
Aysu <aysu.gunerr@gmail.com>
2022-12-18 13:35:10 +0300

    added arrows for assigned random values

--- src/Components/CarouselExchange.js
+++ src/Components/CarouselExchange.js
@@ -1,12 +1,29 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import { Container, Row } from "reactstrap";
-import { FaArrowUp } from "react-icons/fa";
-
+import {
+  FaArrowDown,
+  FaArrowsAltH,
+  FaArrowUp,
+} from "react-icons/fa";
 
 export default class CarouselExhange extends Component {
-  componentDidMount() {
-    this.props.getExchangeRates();
-  }
+  compareValues = (exchangeRate) => {
+    return (
+      <ul className="exchange-ul-style" key={exchangeRate.id}>
+        <li style={{ width: "100px" }}>{exchangeRate.currencyName}</li>
+        <li>
+          {exchangeRate.price}
+          {exchangeRate.status === "increase" ? (
+            <FaArrowUp className="arrow-price" style={{ color: "green" }} />
+          ) : exchangeRate.status === "decrease" ? (
+            <FaArrowDown className="arrow-price" style={{ color: "red" }} />
+          ) : (
+            <FaArrowsAltH className="arrow-price" style={{ color: "grey" }} />
+          )}
+        </li>
+      </ul>
+    );
+  };
 
   render() {
     return (
@@ -14,35 +31,12 @@ export default class CarouselExhange extends Component {
         <Container className="exchange-slideshow">
           <Row className="exchange-card-styles">
             <div style={{ display: "flex" }}>
-              {this.props.exchangeRates.map((exchangeRate) => (
-                <ul className="exchange-ul-style" key={exchangeRate.id}>
-                  <li style={{ width: "100px" }}>
-                    {exchangeRate.currencyName}
-                  </li>
-                  <li>
-                    {exchangeRate.price}
-                    <FaArrowUp
-                      style={{
-                        width: "13px",
-                        color: "green",
-                        marginLeft: "10px",
-                        marginTop: "-5px",
-                      }}
-                    />
-                  </li>
-                </ul>
-              ))}
-              {this.props.exchangeRates.map((exchangeRate) => (
-                <ul className="exchange-ul-style" key={exchangeRate.id}>
-                  <li style={{ width: "100px" }}>
-                    {exchangeRate.currencyName}
-                  </li>
-                  <li>
-                    {exchangeRate.price}
-                    <FaArrowUp className="arrow-price"  />
-                  </li>
-                </ul>
-              ))}
+              {this.props.exchangeRates.map((x) => {
+                return this.compareValues(x);
+              })}
+              {this.props.exchangeRates.map((x) => {
+                return this.compareValues(x);
+              })}
             </div>
           </Row>
         </Container>
